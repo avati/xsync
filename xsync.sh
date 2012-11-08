987af2b8832577399793e29ddf9b3e67fb8c18a1
Anand Avati <avati@redhat.com>
2012-11-08 10:57:56 -0800

    gsyncd/xsync: offer session status in state file
    Signed-off-by: Anand Avati <avati@redhat.com>

--- xsync.sh
+++ xsync.sh
@@ -919,9 +919,9 @@ function keep_idler_busy()
 	read -t $HEARTBEAT_INTERVAL pong <&${COPROC[0]} || break;
 	i=$(($i + 1));
 	sleep 15;
-	echo -n OK > $STATEFILE;
+	echo -n "`hostname`-$SLAVEHOST:OK" > $STATEFILE;
     done
-    echo -n NOK > $STATEFILE;
+    echo -n "`hostname`-$SLAVEHOST:NOK" > $STATEFILE;
 }
 
 
